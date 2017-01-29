578483d9f9435bcbc4c82c42a64a0e4532f99ec7
Charlie Person <charlie.person@hyperisland.se>
2017-01-28 22:44:47 -0500

    Throws IllegalArgumentException when number is below 0- passing all tests

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,7 +1,8 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int high) {
+    public Integer compute(int high) throws IllegalArgumentException {
+        if(high<0) throw new IllegalArgumentException("Number cannot be below 0");
         int product = 1;
         for(int i=1; i<=high; i++) {
             product *= i;
