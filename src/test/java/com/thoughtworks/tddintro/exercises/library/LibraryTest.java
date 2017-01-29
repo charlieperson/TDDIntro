73918171fd136a6ec7c460e02f4756c8002bf795
Charlie Person <charlie.person@hyperisland.se>
2017-01-29 17:49:20 +0000

    Creates passing test checking that nothing is printed if no books have been added

--- src/test/java/com/thoughtworks/tddintro/exercises/library/LibraryTest.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/library/LibraryTest.java
@@ -35,17 +35,14 @@ public class LibraryTest {
     public void shouldPrintBookTitleWhenThereIsOneBook() {
         String title = "Book Title";
         books.add(title);
-
         library.listBooks();
         verify(printStream).println("Book Title\n");
-        // add a verify statement here that shows that the book title was printed by the printStream
     }
 
     @Test
-    @Ignore // Remove each @Ignore and implement test
     public void shouldPrintNothingWhenThereAreNoBooks() {
-
-        // implement me
+        library.listBooks();
+        verify(printStream).println("");
     }
 
     @Test
