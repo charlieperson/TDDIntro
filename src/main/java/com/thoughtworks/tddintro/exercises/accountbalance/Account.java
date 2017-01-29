8b1ea7cfe9750341a974e4cd52fed47a750bea6c
Charlie Person <charlie.person@hyperisland.se>
2017-01-28 22:55:15 -0500

    Adds first failing Account test and fixes compile errors

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,12 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    int balance = 0;
+
+    public void deposit(int amount) {
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -1,13 +1,23 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class AccountTests {
+    Account account;
+
+    @Before
+    public void setUp() {
+        account = new Account();
+    }
+
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+        account.deposit(100);
+        assertThat(account.getBalance(), is(100));
     }
 
     @Test
