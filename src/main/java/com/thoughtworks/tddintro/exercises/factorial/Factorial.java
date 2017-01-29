3e4efe62be4e5fa227d88b4f9944fac122645e3d
Charlie Person <charlie.person@hyperisland.se>
2017-01-28 22:40:27 -0500

    Returns 1 when number is 0- passing first three tests

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,7 +2,7 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int high) {
-        if(high == 1) return 1;
+        if(high == 1 || high == 0) return 1;
         return 2;
     }
 }
