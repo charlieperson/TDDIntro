b826d727242d81e5a6a3c9a5bbe4956a6b7a304f
Charlie Person <charlie.person@hyperisland.se>
2017-01-28 22:43:21 -0500

    Returns 6 when number is 3- passing first four tests

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,7 +2,10 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int high) {
-        if(high == 1 || high == 0) return 1;
-        return 2;
+        int product = 1;
+        for(int i=1; i<=high; i++) {
+            product *= i;
+        }
+        return product;
     }
 }
